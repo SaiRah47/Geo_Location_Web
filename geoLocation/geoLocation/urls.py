5329be43754c6c972ef613443f117f61c8d4e8fe
SaiRah47 <rahulsaipratap@gmail.com>
2020-06-08 12:40:28 +0530

    some other changes

--- geoLocation/geoLocation/urls.py
+++ geoLocation/geoLocation/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import dashboard, logout
+from .views import dashboard, logout, report, profiles
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('logout/', logout, name="logout"),
     path('',dashboard, name="dashboard"),
+    path('report/',report, name="report"),
+    path('profiles/',profiles, name="profiles"),
 ]
